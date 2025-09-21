e330a79c6cc7547c1c579900d1d35fc05f3aa965
Mathieu Dubart <mathieu.dubart@icloud.com>
2025-09-21 19:33:10 -0400

    Initial Commit

---
+++ live-video-preview/live_video_previewApp.swift
@@ -0,0 +1,17 @@
+//
+//  live_video_previewApp.swift
+//  live-video-preview
+//
+//  Created by Mathieu Dubart on 21/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct live_video_previewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
